b7fb83b47c307268f520719421ed6b68378fc8e3
Sebastian <jnssuarez@gmail.com>
2017-01-12 21:59:32 -0500

    - Version inicial

---
+++ New Unity Project/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadGame : MonoBehaviour {
+
+	public GameObject hud;
+	public GameObject city;
+
+	private StartGame sgame;
+
+	private Button btnJugar;
+
+	void Awake(){
+
+		sgame = new StartGame();
+
+		hud = (GameObject)Instantiate(Resources.Load("Prefabs/HUD"), transform.position, transform.rotation);
+		hud.name = "HUD";
+		btnJugar = (Button)GameObject.Find ("Canvas/Panel/btnJugar").GetComponent<Button>();
+
+		btnJugar.onClick.AddListener(()=> Play());
+	}
+
+	public void Play(){
+		sgame.startGame ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/Scripts/StartGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+	public GameObject hud;
+	public GameObject city;
+
+	public Camera CameraMenu;
+	public Camera CameraGame;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void startGame(){
+
+		GameObject.Find ("HUD").active = false;
+		//GameObject.Find ("CameraMenu").active = false;
+
+		CameraMenu =  GameObject.Find ("CameraMenu").GetComponent<Camera>();
+		CameraGame =  GameObject.Find ("CameraGame").GetComponent<Camera>();
+
+		CameraMenu.transform.position = CameraGame.transform.position;
+		CameraMenu.transform.rotation = CameraGame.transform.rotation;
+
+		CameraMenu.orthographicSize = CameraGame.orthographicSize;
+		CameraMenu.nearClipPlane = CameraGame.nearClipPlane;
+
+		GameObject.Find ("CameraGame").active = false;
+
+
+		city = (GameObject) Instantiate(Resources.Load("Prefabs/City"), Vector3.zero, Quaternion.identity);
+
+
+
+
+		//hud = (GameObject)Instantiate(Resources.Load("Prefabs/HUD"), transform.position, transform.rotation);
+
+	}
+}
