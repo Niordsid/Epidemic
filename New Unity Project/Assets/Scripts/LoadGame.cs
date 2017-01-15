9c73ff877fd72315a9ad43c47738622f8e358dfa
ccortizt <ccamilo0203@gmail.com>
2017-01-15 18:56:18 -0500

    - Agregado otro modelo de zombie

--- New Unity Project/Assets/Scripts/LoadGame.cs
+++ New Unity Project/Assets/Scripts/LoadGame.cs
@@ -91,7 +91,10 @@ public class LoadGame : MonoBehaviour {
 		Vector3 zombieOrigin = city.transform.FindChild("ZombieSpawnPoints").FindChild("Point" + randomPoint).transform.position ;
 
 		//zombie = (GameObject) Instantiate(Resources.Load("Prefabs/Zombie"), zombieOrigin, Quaternion.identity);
-        zombie = (GameObject)Instantiate(Resources.Load("Prefabs/Zombie_Lambent_Male"), zombieOrigin, Quaternion.identity);
+        if(Random.Range(0,2) == 0)
+            zombie = (GameObject)Instantiate(Resources.Load("Prefabs/Zombie_Lambent_Male"), zombieOrigin, Quaternion.identity);
+        else
+            zombie = (GameObject)Instantiate(Resources.Load("Prefabs/GreyZombie"), zombieOrigin, Quaternion.identity);
 	}
 
 	public void createSurvivor(){
